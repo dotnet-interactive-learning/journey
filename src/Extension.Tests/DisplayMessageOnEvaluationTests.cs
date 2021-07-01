1c9f217d1a9b6c639a211a352e8fd7acd3a1b862
Cera Samson <cerasamson@gmail.com>
2021-07-01 15:09:32 -0700

    Added evaluation class

---
+++ src/Extension.Tests/DisplayMessageOnEvaluationTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.DotNet.Interactive.Formatting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Extension.Tests
+{
+    public class DisplayMessageOnEvaluationTests
+    {
+        [Fact]
+        public void passing_evaluation_gives_correct_display_string()
+        {
+            // arrange
+            var evaluation = new Evaluation();
+            evaluation.Passed = true;
+            // act
+            var message = evaluation.ToDisplayString(HtmlFormatter.MimeType);
+            // assert
+            message
+                .Should()
+                .Contain("Success");
+        }
+
+
+        [Fact]
+        public void 
+
+
+    }
+}
