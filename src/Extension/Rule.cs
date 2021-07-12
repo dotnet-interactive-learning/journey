f110767e76f317d6d055384ac728bbb417830bb9
Liwam <lytesfazion@dons.usfca.edu>
2021-07-12 11:06:54 -0700

    wip

---
+++ src/Extension/Rule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.DotNet.Interactive;
+
+namespace Extension
+{
+    public class Rule
+    {
+        private readonly Func<KernelCommandResult, bool> ruleContraints;
+
+        public Rule(Func<KernelCommandResult,bool> ruleContraints)
+        {
+            this.ruleContraints = ruleContraints;
+        }
+        internal void TestResult(KernelCommandResult result)
+        {
+            ruleContraints.Invoke(result);
+
+            throw new NotImplementedException();
+        }
+    }
+}
