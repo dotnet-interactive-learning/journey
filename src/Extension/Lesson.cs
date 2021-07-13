8c0d3f889ea268dec7326e0465b83a52e9407c4c
Angle Qian <t-angleqian@microsoft.com>
2021-07-13 13:08:08 -0400

    stage control (#37)
    * stage control
    
    * use currentstage
    
    * fix currentstage to be updated when new stage revealed
    
    * stop operations from throwing
    
    * * goto a stage
    * onfocus listener
    * explicit commits
    
    * * refactor tests
    * make CurrentStage directly assignable
    
    * test refactor
    
    * remove stageid
    
    * rename stage to challenge
    
    * test file rename
    
    * * add lesson
    * add onEvaluationComplete for Challenge
    * test case refactor
    * use orderedset for ChallengeController
    
    * rename OrderedSet -> OrderedHashtable

---
+++ src/Extension/Lesson.cs
@@ -0,0 +1,52 @@
+﻿using Extension.ChallengeControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public class Lesson
+    {
+        public ChallengeController ChallengeController { get; private set; } = new ChallengeController();
+        public Challenge CurrentChallenge
+        {
+            get
+            {
+                return ChallengeController.CurrentChallenge;
+            }
+            set
+            {
+                ChallengeController.CurrentChallenge = value;
+            }
+        }
+
+        private Dictionary<string, Challenge> _challenges = new Dictionary<string, Challenge>();
+
+        public Lesson()
+        {
+
+        }
+
+        public void AddChallenge(string challengeId, Challenge challenge)
+        {
+            if (_challenges.ContainsKey(challengeId))
+            {
+                throw new ArgumentException($"Challenge already exists with id {challengeId}");
+            }
+            _challenges.Add(challengeId, challenge);
+            ChallengeController.AddChallenge(challenge);
+            challenge.Lesson = this;
+        }
+
+        public void GoToChallenge(string challengeId)
+        {
+            if (!_challenges.ContainsKey(challengeId))
+            {
+                throw new ArgumentException($"No challenge exists with id {challengeId}");
+            }
+            CurrentChallenge = _challenges[challengeId];
+        }
+    }
+}
