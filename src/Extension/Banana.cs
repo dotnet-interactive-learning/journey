6be933fadc1af4be7cbad66e366f0a2ddbd7ac12
Liwam <lytesfazion@dons.usfca.edu>
2021-07-12 17:51:29 -0700

    Made "when_the_output_fails..." test pass

---
+++ src/Extension/Banana.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Extension
+{
+    public class Banana
+    {
+        public bool Passed { get;private set; }
+
+        public void Fail()
+        {
+            this.Passed = false;
+
+            
+        }
+
+        public void Pass()
+        {
+            this.Passed = true;
+
+
+        }
+    }
+
+}
