007c145901219c7aab59ceb2bfa8e5f9ec860086
Angle Qian <t-angleqian@microsoft.com>
2021-07-15 17:29:15 -0400

    challengecontext can access submission information

---
+++ src/Extension/ChallengeSubmission.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.DotNet.Interactive.Events;
+using System.Collections.Generic;
+
+namespace Extension
+{
+    public class ChallengeSubmission
+    {
+        public string SubmissionCode { get; private set; }
+        public Evaluation Evaluation { get; private set; }
+        public IEnumerable<KernelEvent> EventsProduced { get; private set; }
+        public IEnumerable<Evaluation> RuleEvaluations => Evaluation.RuleEvaluations;
+
+        public ChallengeSubmission(string submissionCode, Evaluation evaluation, IEnumerable<KernelEvent> events)
+        {
+            SubmissionCode = submissionCode;
+            Evaluation = evaluation;
+            EventsProduced = events;
+        }
+    }
+}
