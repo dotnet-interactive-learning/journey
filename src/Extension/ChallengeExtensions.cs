4a674af4827811b2bbb3bce0f6ad481d46eb61a9
Liwam <lytesfazion@dons.usfca.edu>
2021-07-23 15:36:12 -0700

    missing files

---
+++ src/Extension/ChallengeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class ChallengeExtensions
+    {
+        public static Challenge SetDefaultProgression(this Challenge challenge, Challenge nextChallenge) 
+        {
+            challenge.DefaultOnCodeSubmittedHandler = async context => await context.StartChallengeAsync(nextChallenge);
+            return nextChallenge;
+        }
+
+        public static void SetDefaultProgression(this IEnumerable<Challenge> challenges)
+        {
+            var cs = challenges.ToList();
+            for(int i = 0; i < cs.Count-1; i++)
+            {
+                cs[i].SetDefaultProgression(cs[i+1]);
+            }
+        }
+    }
+}
